6655adf1516feff13cdb516fa4d512593e40d198
Miguel Gandia <miguelgandiajorda@gmail.com>
2025-01-19 20:25:23 +0100

    linter

--- eslint.config.js
+++ eslint.config.js
@@ -1,38 +1,19 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
-import pluginJest from "eslint-plugin-jest"; // Para soporte de Jest
-import pluginReactHooks from "eslint-plugin-react-hooks"; // Soporte para hooks de React
+import react from 'eslint-plugin-react';
 
-/** @type {import('eslint').Linter.Config} */
-const config = [
-  {
-    files: ["**/*.{js,mjs,cjs,jsx}"],
-    languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-      globals: {
-        ...globals.browser, // Soporte para el navegador
-        ...globals.node, // Soporte para Node.js
-      },
-      ecmaFeatures: {
-        jsx: true, // Habilitar JSX
-      },
-    },
-    plugins: {
-      react: pluginReact,
-      "react-hooks": pluginReactHooks,
-      jest: pluginJest,
-    },
-    rules: {
-      ...pluginJs.configs.recommended.rules, // Reglas recomendadas por ESLint
-      ...pluginReact.configs.recommended.rules, // Reglas recomendadas para React
-      ...pluginReactHooks.configs.recommended.rules, // Reglas recomendadas para React Hooks
-      ...pluginJest.configs.recommended.rules, // Reglas recomendadas para Jest
-      "react/react-in-jsx-scope": "off", // React 17+ no requiere importar React
-      "react/jsx-no-target-blank": "warn", // Seguridad para enlaces
-    },
-  },
+export default [
+     {
+          files: ['**/*.{js,jsx}'],
+          plugins: { react },
+          languageOptions: {
+               ecmaVersion: 'latest',
+               sourceType: 'module',
+               parserOptions: {
+                    ecmaFeatures: { jsx: true },
+               },
+          },
+          rules: {
+               'react/jsx-uses-react': 'error',
+               'react/jsx-uses-vars': 'error',
+          },
+     },
 ];
-
-export default config;
