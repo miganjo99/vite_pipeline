23ce8e9e9514a4734ec52d828d64e079f7f46cef
Miguel Gandia <miguelgandiajorda@gmail.com>
2025-01-19 23:05:13 +0100

    update

--- misScripts/updateReadme.cjs
+++ misScripts/updateReadme.cjs
@@ -1,19 +1,19 @@
-const fs = require('fs');
+import fs from 'fs';
 
 const readmePath = './README.md';
 
 const successBadge = '![Success](https://img.shields.io/badge/tested%20with-Cypress-04C38E.svg)';
 const failureBadge = '![Failure](https://img.shields.io/badge/test-failure-red)';
 
-const testResult = 'success';
+const testResult = process.argv[2] === 'Correcto' ? 'success' : 'failure';
 
 const badge = testResult === 'success' ? successBadge : failureBadge;
 
 const readmeContent = fs.existsSync(readmePath) ? fs.readFileSync(readmePath, 'utf-8') : '';
 
 const updatedContent = readmeContent.includes('Resultado de los últimos tests')
-     ? readmeContent.replace(/Resultados de los tests.*/s, `Resultaods\n\n${badge}`)
-     : `${readmeContent}\n\n## Resultados de los tests\n\n${badge}`;
+  ? readmeContent.replace(/Resultados de los tests.*/s, `Resultados de los tests\n\n${badge}`)
+  : `${readmeContent}\n\n## Resultados de los tests\n\n${badge}`;
 
 fs.writeFileSync(readmePath, updatedContent, 'utf-8');
 
